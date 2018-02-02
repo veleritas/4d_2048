c60e7f1a2ee80dd785f71e2ac8bd7c44418f5ffd
Tong Shu Li <tongli@scripps.edu>
2018-02-01 18:00:45 -0800

    Convert board representation to an integer
    Convert the internal representation of the board state from a 2D
    array to a 64 bit unsigned integer.

--- gui.cpp
+++ gui.cpp
@@ -4,38 +4,46 @@ using namespace std;
 
 const int CELLS = 16;
 
-int state[CELLS];
-
 std::random_device rd;
 std::mt19937 gen(rd());
 std::uniform_int_distribution<> dist(0, 9); // [0, 9]
 
-int new_tile()
+uint64_t new_tile()
 {
     return dist(gen) < 9? 1 : 2;
 }
 
-void init_board()
+uint64_t get_val(uint64_t value, int pos)
 {
+    // Return the value at a given position.
+    uint64_t mask = 0xFULL << (4 * pos);
+    return (value & mask) >> (4 * pos);
+}
+
+uint64_t init_board()
+{
+    // Return starting state of game.
+
     std::uniform_int_distribution<> temp(0, CELLS-1);
 
     int pos[2] = {0};
     while (pos[0] == pos[1])
         pos[0] = temp(gen), pos[1] = temp(gen);
 
-    state[pos[0]] = new_tile();
-    state[pos[1]] = new_tile();
+    return (new_tile() << (4 * pos[0])) | (new_tile() << (4 * pos[1]));
 }
 
-void draw_board()
+void draw_board(uint64_t state)
 {
+    // Given a game state value, draw the board with ASCII art.
+
     puts("+--+--+");
     for (int i=0; i<CELLS; i++)
     {
         if (i % 2 == 0)
             printf("|");
 
-        printf("%x", state[i]);
+        printf("%x", get_val(state, i));
 
         if (i % 4 == 3)
             puts("|");
@@ -43,15 +51,12 @@ void draw_board()
         if (i % 8 == 7)
             puts("+--+--+");
     }
-
-    printf("\n");
 }
-
 
 int main()
 {
-    init_board();
-    draw_board();
+    uint64_t state = init_board();
+    draw_board(state);
 
     return 0;
 }
