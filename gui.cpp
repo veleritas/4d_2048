69267abe0965ad3e17029a979e116a26369ee1f3
Tong Shu Li <tongli@scripps.edu>
2018-02-06 17:45:58 -0800

    Fix lookup table generation for moving left
    Fix a bug with lookup table generation that only considered merging
    tiles when moving left. Now it also correctly moves tiles when the
    spot to the left is empty.
    
    Create a function which moves the entire board to the left.

--- gui.cpp
+++ gui.cpp
@@ -2,26 +2,26 @@
 #include <random>
 using namespace std;
 
+typedef uint64_t board_t;
+typedef uint16_t row_t;
+
 static const int CELLS = 16;
 static const int MAX_VALS = 65536; // 2^16
+
+static const uint16_t ROW_MASK = 0xFFFF;
 
 std::random_device rd;
 std::mt19937 gen(rd());
 std::uniform_int_distribution<> dist(0, 9); // [0, 9]
 
-// Store move lookup table for moving left arrow key
-uint16_t move_L[MAX_VALS];
-uint16_t move_R[MAX_VALS];
+// Store move lookup tables
+
+row_t move_L[MAX_VALS];
+row_t move_R[MAX_VALS];
 
 uint64_t new_tile()
 {
     return dist(gen) < 9? 1 : 2;
-}
-
-uint64_t get_val(uint64_t value, int pos)
-{
-    // Return the value at a given position.
-    return (value >> (4 * pos)) & 0xF;
 }
 
 uint64_t spawn_tile(uint64_t state)
@@ -38,7 +38,7 @@ uint64_t spawn_tile(uint64_t state)
     return state | (new_tile() << (4 * pos));
 }
 
-void draw_board(uint64_t state)
+void draw_board(board_t state)
 {
     // Given a game state value, draw the board with ASCII art.
 
@@ -48,7 +48,8 @@ void draw_board(uint64_t state)
         if (i % 2 == 0)
             printf("|");
 
-        printf("%x", get_val(state, i));
+        printf("%x", state & 0xF);
+        state >>= 4;
 
         if (i % 4 == 3)
             puts("|");
@@ -82,10 +83,14 @@ void init_L()
         uint16_t lower = i & 0xFF;
         uint16_t upper = i >> 8;
 
-        if ((lower > 0 && lower < 0xFF) && ((lower & 0xF) == (lower >> 4)))
-            lower = (lower & 0xF) + 1;
+        if ((lower & 0xF) == 0)
+            lower >>= 4; // Move tile to empty spot
+        else if ((lower < 0xFF) && ((lower & 0xF) == (lower >> 4)))
+            lower = (lower & 0xF) + 1; // Combine tiles
 
-        if ((upper > 0 && upper < 0xFF) && ((upper & 0xF) == (upper >> 4)))
+        if ((upper & 0xF) == 0)
+            upper >>= 4;
+        else if ((upper < 0xFF) && ((upper & 0xF) == (upper >> 4)))
             upper = (upper & 0xF) + 1;
 
         move_L[i] = (upper << 8) | lower;
@@ -107,23 +112,56 @@ void init_moves()
     init_R();
 }
 
+static inline board_t move_board_L(board_t state)
+{
+    // Move the entire board left.
+    board_t res = 0;
+
+    res |= board_t(move_L[(state >> 0) & ROW_MASK]) << 0;
+    res |= board_t(move_L[(state >> 16) & ROW_MASK]) << 16;
+    res |= board_t(move_L[(state >> 32) & ROW_MASK]) << 32;
+    res |= board_t(move_L[(state >> 48) & ROW_MASK]) << 48;
+
+    return res;
+}
+
+board_t rand_start()
+{
+    // Create a randomly filled board for debugging purposes.
+
+    std::uniform_int_distribution<> dist(0, MAX_VALS-1);
+
+    board_t res = 0;
+    for (int i=0; i<4; i++)
+        res |= board_t(dist(gen)) << (16 * i);
+
+    return res;
+}
+
 int main()
 {
     init_moves();
 
-    uint16_t hi = 0xaa01;
+    board_t state = rand_start();
+    draw_board(state);
 
-    draw_row(hi);
-    draw_row(move_L[hi]);
-    draw_row(move_R[hi]);
+    state = move_board_L(state);
+    draw_board(state);
 
     return 0;
 
+/*
+    init_moves();
 
-    init_L();
-
-    uint64_t state = spawn_tile(spawn_tile(0));
+    board_t state = spawn_tile(spawn_tile(0));
     draw_board(state);
+
+    cout << endl;
+
+    state = move_board_L(state);
+    draw_board(state);
+
+*/
 
     return 0;
 }
